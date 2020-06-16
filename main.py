94f5ec1bc50dbd2600f2f41ca0912c37f5027d74
Aan Subarkah <aansubarkah@gmail.com>
2020-06-16 18:11:01 +0800

    templating

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 @app.get("/")
-def dashboard():
-    return{"Dashboard": "World"}
+def home(request: Request):
+    '''
+    displays the stock screener dashboard/homepage
+    '''
+    #return{"Dashboard": "World"}
+    return templates.TemplateResponse("home.html", {
+        "request": request,
+        "somevar": 2
+    })
+
+@app.post("/stock")
+def create_stock():
+    '''
+    created a stock and stores it in the database
+    '''
+    return {
+        "code": "success",
+        "message": "stock created"
+    }
